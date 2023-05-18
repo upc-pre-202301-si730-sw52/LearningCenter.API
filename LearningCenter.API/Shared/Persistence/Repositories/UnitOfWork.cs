3c23624b3e335c4a0ce31f9a09fa8420e2c5edec
Angel Velasquez <pcisavel@upc.edu.pe>
2023-05-17 21:11:27 -0500

    chore: added unit of work concrete class.

---
+++ LearningCenter.API/Shared/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+using LearningCenter.API.Shared.Domain.Repositories;
+using LearningCenter.API.Shared.Persistence.Contexts;
+
+namespace LearningCenter.API.Shared.Persistence.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+    
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
