58fee171d1053a5aeb536876c9a567cc64f36aae
Angel Velasquez <pcisavel@upc.edu.pe>
2023-05-17 21:43:36 -0500

    feat: implemented category service.

---
+++ LearningCenter.API/Learning/Services/CategoryService.cs
@@ -0,0 +1,82 @@
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Repositories;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Domain.Services.Communication;
+using LearningCenter.API.Shared.Domain.Repositories;
+
+namespace LearningCenter.API.Learning.Services;
+
+public class CategoryService : ICategoryService
+{
+    
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CategoryService(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+    {
+        _categoryRepository = categoryRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<IEnumerable<Category>> ListAsync()
+    {
+        return await _categoryRepository.ListAsync();
+    }
+
+    public async Task<CategoryResponse> SaveAsync(Category category)
+    {
+        try
+        {
+            await _categoryRepository.AddAsync(category);
+            await _unitOfWork.CompleteAsync();
+
+            return new CategoryResponse(category);
+        }
+        catch (Exception e)
+        {
+            return new CategoryResponse($"An error occurred while saving the category: {e.Message}");
+        }
+    }
+
+    public async Task<CategoryResponse> UpdateAsync(int id, Category category)
+    {
+        var existingCategory = await _categoryRepository.FindByIdAsync(id);
+        
+        if (existingCategory == null)
+            return new CategoryResponse("Category not found.");
+        
+        existingCategory.Name = category.Name;
+
+        try
+        {
+            _categoryRepository.Update(existingCategory);
+            await _unitOfWork.CompleteAsync();
+
+            return new CategoryResponse(existingCategory);
+        }
+        catch (Exception e)
+        {
+            return new CategoryResponse($"An error occurred while updating the category: {e.Message}");
+        }
+    }
+
+    public async Task<CategoryResponse> DeleteAsync(int id)
+    {
+        var existingCategory = await _categoryRepository.FindByIdAsync(id);
+        
+        if (existingCategory == null)
+            return new CategoryResponse("Category not found.");
+
+        try
+        {
+            _categoryRepository.Remove(existingCategory);
+            await _unitOfWork.CompleteAsync();
+
+            return new CategoryResponse(existingCategory);
+        }
+        catch (Exception e)
+        {
+            return new CategoryResponse($"An error occurred while deleting the category: {e.Message}");
+        }
+    }
+}
