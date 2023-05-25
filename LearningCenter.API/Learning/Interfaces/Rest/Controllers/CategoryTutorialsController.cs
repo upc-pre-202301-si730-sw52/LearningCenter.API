1b9d81338624f91ba3123b7d264a1fa4ec78a436
Angel Velasquez <pcisavel@upc.edu.pe>
2023-05-24 21:22:43 -0500

    feat: added nested resource for category tutorials.

---
+++ LearningCenter.API/Learning/Interfaces/Rest/Controllers/CategoryTutorialsController.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningCenter.API.Learning.Interfaces.Rest.Controllers;
+
+[ApiController]
+[Route("/api/v1/categories/{categoryId}/tutorials")]
+public class CategoryTutorialsController : ControllerBase
+{
+    private readonly ITutorialService _tutorialService;
+    private readonly IMapper _mapper;
+
+    public CategoryTutorialsController(ITutorialService tutorialService, IMapper mapper)
+    {
+        _tutorialService = tutorialService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<TutorialResource>> GetAllByCategoryId(int categoryId)
+    {
+        var tutorials = await _tutorialService.ListByCategoryIdAsync(categoryId);
+        var resources = _mapper.Map<IEnumerable<Tutorial>, IEnumerable<TutorialResource>>(tutorials);
+        return resources;
+    }
+}
