563560b9400c9ac4f68b81fb9c9d1a6fd86d466a
Angel Velasquez <pcisavel@upc.edu.pe>
2023-05-24 20:48:46 -0500

    chore: added application configuration.

--- LearningCenter.API/Program.cs
+++ LearningCenter.API/Program.cs
@@ -1,3 +1,12 @@
+using LearningCenter.API.Learning.Domain.Repositories;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Persistence.Repositories;
+using LearningCenter.API.Learning.Services;
+using LearningCenter.API.Shared.Domain.Repositories;
+using LearningCenter.API.Shared.Persistence.Contexts;
+using LearningCenter.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +16,51 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
+// Add Database Connection
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
+// Lowercase URLs configuration
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Dependency Injection Configuration
+
+// Shared Bounded Contexts Injection Configuration
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Learning Bounded Context Injection Configuration
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+// AutoMapper Configuration
+
+builder.Services.AddAutoMapper(
+    typeof(LearningCenter.API.Learning.Mapping.ModelToResourceProfile),
+    typeof(LearningCenter.API.Learning.Mapping.ResourceToModelProfile));
+
+// Application build
+
 var app = builder.Build();
+
+
+// Validation for ensuring Database Objects are created
+
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+{
+    context.Database.EnsureCreated();
+}
+        
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
