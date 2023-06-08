d8b07691af1e70fada0b5354a2ab54cf4e6d5be9
Angel Velasquez <pcisavel@upc.edu.pe>
2023-06-07 20:14:34 -0500

    feat: added Exception class.

---
+++ LearningCenter.API/Security/Exceptions/AppException.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+
+namespace LearningCenter.API.Security.Exceptions;
+
+public class AppException : Exception
+{
+    public AppException() : base()
+    {
+        
+    }
+
+    public AppException(string? message) : base(message)
+    {
+    }
+
+    public AppException(string? message, params object[] args) :
+        base(string.Format(CultureInfo.CurrentCulture, message, args))
+    {
+    }
+}
