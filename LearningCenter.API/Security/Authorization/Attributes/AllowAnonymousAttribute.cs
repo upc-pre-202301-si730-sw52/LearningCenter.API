e29a3a932ed72db131b52cfebcd2f4ff33b830a8
Angel Velasquez <pcisavel@upc.edu.pe>
2023-06-07 20:31:37 -0500

    feat: added Allow Anonymous and Authorize attributes.

---
+++ LearningCenter.API/Security/Authorization/Attributes/AllowAnonymousAttribute.cs
@@ -0,0 +1,7 @@
+namespace LearningCenter.API.Security.Authorization.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class AllowAnonymousAttribute : Attribute
+{
+    
+}
---
+++ LearningCenter.API/Security/Authorization/Attributes/AuthorizeAttribute.cs
@@ -0,0 +1,31 @@
+using LearningCenter.API.Security.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LearningCenter.API.Security.Authorization.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        // If action is decorated with [AllowAnonymous] attribute
+        var allowAnonymous = context
+            .ActionDescriptor
+            .EndpointMetadata
+            .OfType<AllowAnonymousAttribute>()
+            .Any();
+
+        if (allowAnonymous)
+            // Then skip authorization process and return
+            return;
+        // Otherwise, perform authorization process
+        var user = (User)context.HttpContext.Items["User"];
+        if (user == null)
+            context.Result = new JsonResult(
+                new { message = "Unauthorized" })
+            {
+                StatusCode = StatusCodes.Status401Unauthorized
+            };
+    }
+}
